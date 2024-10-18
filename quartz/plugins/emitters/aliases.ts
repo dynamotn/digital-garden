b457fba34dae0a5ed39ca4ec8c1a891e96d8f244
Trần Đức Nam <me@dynamotn.dev>
2024-10-18 12:29:44 +0700

    feat: parse multilanguage content

--- quartz/plugins/emitters/aliases.ts
+++ quartz/plugins/emitters/aliases.ts
@@ -42,7 +42,9 @@ export const AliasRedirects: QuartzEmitterPlugin = () => ({
       const ogSlug = simplifySlug(file.data.slug!)
       const dir = path.posix.relative(argv.directory, path.dirname(file.data.filePath!))
       const aliases = file.data.frontmatter?.aliases ?? []
-      const slugs: FullSlug[] = aliases.map((alias) => path.posix.join(ctx.language, dir, alias) as FullSlug)
+      const slugs: FullSlug[] = aliases.map(
+        (alias) => path.posix.join(ctx.language, dir, alias) as FullSlug,
+      )
       const permalink = file.data.frontmatter?.permalink
       if (typeof permalink === "string") {
         slugs.push(permalink as FullSlug)
