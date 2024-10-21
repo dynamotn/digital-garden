f97dd93620a55f3968fcebd10a87331fd465d8de
Trần Đức Nam <me@dynamotn.dev>
2024-10-22 00:10:28 +0700

    refactor: remove article title

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,7 +20,6 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
-    Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
     Component.Banner(),
